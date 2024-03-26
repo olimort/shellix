35e8bc6107a4a84da724f358ae9502c4883e2272
Thiago Oliveira <thiagomoraisee@gmail.com>
2024-03-26 18:02:16 -0300

    Fix multiple branches print

--- shellix.sh
+++ shellix.sh
@@ -1,6 +1,6 @@
-SHELLIX_HOME=$HOME/shellix
+SHELLIX_HOME=$HOME/Repositórios/shellix
 source $SHELLIX_HOME/bashcolors.sh
 
-PS1=$(normal [)$(blue '\u' 1)$(normal '@')$(cyan '\h' 1)$(normal '][')$(green '\w')$(normal ']')$(yellow)'$(git branch 2>/dev/null)'$(normal '\n →')' '
+PS1=$(normal [)$(blue '\u' 1)$(normal '@')$(cyan '\h' 1)$(normal '][')$(green '\w')$(normal ']')$(yellow)'$(git branch --show-current 2>/dev/null)'$(normal '\n →')' '
 
 export PS1
